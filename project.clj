8e3a540b2112fd51246322911bd614ee071f9f87
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2016-01-15 18:07:23 +0000

    Changed signature email

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ducky427/electron-template"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :signing {:gpg-key "rohit@keybase.io"}
+  :signing {:gpg-key "rohit.aggarwal@gmail.com"}
   :scm {:name "git"
         :url "https://github.com/ducky427/electron-template"}
   :eval-in-leiningen true)
